0d4d07b946a6617bc04f5e9af4e03efe7d39bebe
pauldev1128 <pauldev1128@gmail.com>
2019-08-07 16:01:19 -0700

    create tables for database

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,33 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
-
 USE chat;
 
+DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
   /* Describe your table here.*/
+  username varchar(20),
+  -- timedate datetime(),
+  ID int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(ID)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
+DROP TABLE IF EXISTS friends;
+CREATE TABLE friends (
+  username varchar(20),
+  statusmsg varchar(20),
+  place varchar(20),
+  ID int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(ID)
+);
+DROP TABLE IF EXISTS myprofile;
+CREATE TABLE myprofile (
+  birthday date,
+  age int,
+  hobby varchar(20),
+  ID int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(ID)
+)
 
 
 /*  Execute this file from the command line by typing:
